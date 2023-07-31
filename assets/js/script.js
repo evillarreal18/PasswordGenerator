2ac11cce71842105b0ebe5fbe2525f11513b09c5
evillarreal18 <ericvillarreal81997@gmail.com>
2023-07-30 20:39:27 -0500

    fixed up the password generator to complete the requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,67 @@
 // Assignment Code
-var passLength = 15;
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lower = 'abcdefghijklmnopqrstuvwxyz';
-var number = '0123456789';
-var symbol = '!@#$%^&*(){}[]-=<>';
 
 
-var randomPassword = upper + lower + number + symbol;
+function gen() {
 
-
-
-var generateBtn = document.querySelector('#generate');
-var passwordText = document.querySelector('#password');
-// Write password to the #password input
-function writePassword() {
-	var password = '';
+	let characters = {
+		upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+		lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+		number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+		special: ['!', ')', '&', '^', '$', '*', '+', '-', '/', '?', '(', '%', '.']
+	};
 	
-	password += upper[Math.floor(Math.random() * upper.length)];
-	password += lower[Math.floor(Math.random() * lower.length)];
-	password += number[Math.floor(Math.random() * number.length)];
-	password += symbol[Math.floor(Math.random() * symbol.length)];
+	var passLength = prompt('choose password length (must be between 8 and 128 characters).');
+	var passwordLengthNum = Number(passLength);
+
+	if (passwordLengthNum < 8 || passwordLengthNum > 128) {
+		prompt('please choose a character length between 8 and 128.');
+	} 
 
 
-	while(passLength > password.length) {
-		password += randomPassword[Math.floor(Math.random() * randomPassword.length)]
+	var uppercase = confirm('Do you want your password to contain uppercase characters?');
+	var lowercase = confirm('Do you want your password to contain lowercase characters?');
+	var num = confirm('Do you want your password to contain numeric characters?');
+	var symbol = confirm('Do you want your password to contain special characters?');
+
+	var passwordGen = [];
+
+	
+	 if (uppercase) {
+	 	passwordGen = passwordGen.concat(characters.upper);
+	 }
+
+	if (lowercase) {
+		passwordGen = passwordGen.concat(characters.lower);
+	}
+	if (num) {
+		passwordGen = passwordGen.concat(characters.number);
 	}
 
-	passwordText.value = password;
-	writePassword();
+	if (symbol) {
+		passwordGen = passwordGen.concat(characters.special);
+	}
+	
+	let password = [];
+
+	for (let i = 0; i < passwordLengthNum; i++) {
+		let randomNum = Math.floor(Math.random()*passwordGen.length);
+		let randomChar = passwordGen[randomNum];
+		password.push(randomChar);
+	}
+
+	
+	
+	return password.join('');
+	
 }
 
-// Add event listener to generate button
+
+function writePassword() {
+	var password = gen();
+  
+	var passwordText = document.querySelector('#password');
+	passwordText.value = password;
+  }
+
+var generateBtn = document.querySelector('#generate');
 generateBtn.addEventListener('click', writePassword);
